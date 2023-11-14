197ed027ef8a3b5d62fbc333b354c0f48076b9bc
vanhuyhvh2k4 <vanhuyhvh2004@gmail.com>
2023-11-14 19:26:43 +0700

    test

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -1,29 +1,40 @@
 <template>
-    <div class="w-full h-20 px-8 py-4 flex justify-between items-center">
-        <section class="flex items-center gap-6">
-            <font-awesome-icon class="text-2xl" :icon="['fas', 'bars']" />
-            <h2 class="text-2xl font-medium">Dashboard</h2>
-        </section>
-        <section class="w-[30%] h-full">
-            <div class="w-full h-full bg-gray-100 rounded-xl flex items-center px-6">
-                <input class="h-full bg-transparent w-full outline-none text-lg" type="text" placeholder="Search Task...">
-                <font-awesome-icon class="cursor-pointer" :icon="['fas', 'search']" />
-            </div>
-        </section>
-        <section class="h-full flex gap-10 items-center">
-            <div class="relative">
-                <font-awesome-icon class="text-3xl" :icon="['fas', 'bell']" />
-                <div class="w-5 h-5 text-sm rounded-full bg-primary grid place-items-center absolute top-[-30%] right-[-12px]">12</div>
-            </div>
-            <div class="h-full flex gap-4 items-center">
-                <div class="text-end">
-                    <h3 class="text-lg font-medium">Emplanner User</h3>
-                    <small class="text-gray-500">IT Student</small>
-                </div>
-                <div class="w-auto h-full">
-                    <img class="w-full h-full flex-shrink-0 object-contain rounded-full" src="https://th.bing.com/th/id/OIP.audMX4ZGbvT2_GJTx2c4GgHaHw?w=182&h=190&c=7&r=0&o=5&pid=1.7" alt="">
-                </div>
-            </div>
-        </section>
-    </div>
+  <div class="w-full h-20 px-8 py-4 flex justify-between items-center">
+    <section class="flex items-center gap-6">
+      <font-awesome-icon class="text-2xl" :icon="['fas', 'bars']" />
+      <h2 class="text-2xl font-medium">Dashboard</h2>
+    </section>
+    <section class="w-[30%] h-full">
+      <div class="w-full h-full bg-gray-100 rounded-xl flex items-center px-6">
+        <input
+          class="h-full bg-transparent w-full outline-none text-lg"
+          type="text"
+          placeholder="Search Task..."
+        />
+        <font-awesome-icon class="cursor-pointer" :icon="['fas', 'search']" />
+      </div>
+    </section>
+    <section class="h-full flex gap-10 items-center">
+      <div class="relative">
+        <font-awesome-icon class="text-3xl" :icon="['fas', 'bell']" />
+        <div
+          class="w-5 h-5 text-sm rounded-full bg-primary grid place-items-center absolute top-[-30%] right-[-12px]"
+          >12</div
+        >
+      </div>
+      <div class="h-full flex gap-4 items-center">
+        <div class="text-end">
+          <h3 class="text-lg font-medium">Emplanner User</h3>
+          <small class="text-gray-500">IT Student</small>
+        </div>
+        <div class="w-auto h-full">
+          <img
+            class="w-full h-full flex-shrink-0 object-contain rounded-full"
+            src="https://th.bing.com/th/id/OIP.audMX4ZGbvT2_GJTx2c4GgHaHw?w=182&h=190&c=7&r=0&o=5&pid=1.7"
+            alt=""
+          />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
--- src/components/Sidebar/Sidebar.vue
+++ src/components/Sidebar/Sidebar.vue
@@ -1,33 +1,39 @@
 <template>
-    <div class="w-2/12 h-full py-6 pl-6 pr-4">
-        <h1 class="text-4xl font-black">
-            <span>EMPLANN</span>
-            <span class="text-primary">ER.</span>
-        </h1>
-        <ul class="mt-20">
-            <Item class="mb-2" @click="handleClick(index)" :isActive="this.indexActive === index" :title="item.title"
-                :icon="this.indexActive === index ? item.icon_active : item.icon" v-for="(item, index) in items"
-                :key="index" />
-        </ul>
-    </div>
+  <div class="w-2/12 h-full py-6 pl-6 pr-4">
+    <h1 class="text-4xl font-black">
+      <span>EMPLANN</span>
+      <span class="text-primary">ER.</span>
+    </h1>
+    <ul class="mt-20">
+      <Item
+        class="mb-2"
+        @click="handleClick(index)"
+        :isActive="this.indexActive === index"
+        :title="item.title"
+        :icon="this.indexActive === index ? item.icon_active : item.icon"
+        v-for="(item, index) in items"
+        :key="index"
+      />
+    </ul>
+  </div>
 </template>
 
 <script>
-import Item from '@/components/Item/Item.vue';
-import svgs from '@/assets/svg/export.js';
-import items from '@/data/sidebar.js';
-export default {
-    name: 'Sidebar',
+  import Item from "@/components/Item/Item.vue";
+  import svgs from "@/assets/svg/export.js";
+  import items from "@/data/sidebar.js";
+  export default {
+    name: "Sidebar",
     data() {
-        return { svgs, items, indexActive: 0 }
+      return { svgs, items, indexActive: 0 };
     },
     components: {
-        Item,
+      Item,
     },
     methods: {
-        handleClick(index) {
-            this.indexActive = index;
-        }
+      handleClick(index) {
+        this.indexActive = index;
+      },
     },
-}
+  };
 </script>
